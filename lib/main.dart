c51b4e3cea9de4c1017cbf201f175f514528c2c7
Geoffrey <agyaregeoffrey@gmail.com>
2021-09-07 12:01:53 +0000

    Learned about Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-          child: Text("Welcome to 30 days of Flutter"),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      routes: {
+        '/login': (context) => LoginPage(),
+      },
     );
   }
 }
