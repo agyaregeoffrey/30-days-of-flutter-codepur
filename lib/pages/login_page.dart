4688d74d91774c98cac5791be2b5818e296eb4ab
Geoffrey <agyaregeoffrey@gmail.com>
2021-09-07 17:06:25 +0000

    Day 3 complete

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/utils.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Login Page'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 32.0),
+        child: Column(
+          children: [
+            Image.asset(
+              'assets/images/login_image.png',
+              fit: BoxFit.contain,
+            ),
+            verticalSpacing(),
+            Text(
+              'Welcome',
+              style: TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            verticalSpacing(),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Username',
+              ),
+            ),
+            verticalSpacing(),
+            TextFormField(
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password',
+              ),
+            ),
+            verticalSpacing(),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Login'),
+              style: ElevatedButton.styleFrom(),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/utils.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Widget verticalSpacing({double space = 20.0}) {
+  return SizedBox(
+    height: space,
+  );
+}
